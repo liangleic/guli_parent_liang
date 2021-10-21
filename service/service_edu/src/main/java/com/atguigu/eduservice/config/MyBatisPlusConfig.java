4659409ce244e818ff7c046563658cc0a53fd0d4
liangleic <liangleic@163.com>
2021-10-21 18:59:32 +0800

    Initial commit

---
+++ service/service_edu/src/main/java/com/atguigu/eduservice/config/MyBatisPlusConfig.java
@@ -0,0 +1,25 @@
+package com.atguigu.eduservice.config;
+
+/**
+ * @Description: MyBatisPlusConfig$
+ * @Author liang
+ * @Date: 2021/10/20$ 11:22$
+ * @Version 1.0
+ */
+
+import com.baomidou.mybatisplus.core.injector.ISqlInjector;
+import com.baomidou.mybatisplus.extension.injector.LogicSqlInjector;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@MapperScan("com.atguigu.eduservice.mapper")
+public class MyBatisPlusConfig {
+
+    @Bean
+    public ISqlInjector sqlInjector() {
+        return new LogicSqlInjector();
+    }
+}
